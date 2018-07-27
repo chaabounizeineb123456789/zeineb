2674544a1fba24879e20c80cf660a7b8dd011915
chaabounizeineb123456789 <41632663+chaabounizeineb123456789@users.noreply.github.com>
2018-07-27 13:26:14 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function italic(){
+    document.getElementById("demo").style.fontStyle="italic";
+}
+function bold(){
+	document.getElementById("demo").style.fontSize="30px";
+}
+function underline(){
+	document.getElementById("demo").style.textDecoration="underline";
+}
+function arial(){
+	document.getElementById("demo").style.fontFamily=document.getElementById("s").value;
+}
+function kabber(){
+	document.getElementById("demo").style.fontSize=document.getElementById("s2").value;
+}
+function loun(){
+	document.getElementById("demo").style.backgroundColor=document.getElementById("s3").value;
+}
+
